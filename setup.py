85745c6503fc16a5dd15e29289e52d56bb59e2df
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-14 22:52:21 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+# standard library
+from setuptools import setup, find_packages
+
+
+# main
+setup(
+    name = 'decode',
+    version = '0.0',
+    url = 'https://github.com/deshima-dev/decode',
+    packages = find_packages(),
+)
