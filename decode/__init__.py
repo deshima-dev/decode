4e9cece5527cae6f8e6f9c52e0759da63f09f25c
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-08 15:02:04 +0900

    Update __init__.py

--- decode/__init__.py
+++ decode/__init__.py
@@ -1,5 +1,17 @@
 # coding: utf-8
 
 # information
-__version__ = 'v0.0'
-__author__  = 'Akio Taniguchi'
+__version__ = '0.1'
+__author__  = 'snoopython'
+__email__   = 'taniguchi@ioa.s.u-tokyo.ac.jp'
+
+from .array import *
+from .logging import *
+from . import utils
+
+# default logger
+import logging
+logger = logging.getLogger('decode')
+setlogfile(logger=logger)
+setloglevel(logger=logger)
+del logging
