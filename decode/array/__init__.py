066097361499b445acbcca80481ca06cc6d9a26b
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-08 15:01:10 +0900

    Add __init__.py

---
+++ decode/array/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from .classes import *
+from .decorators import *
+from .functions import *
+del classes
+del decorators
+del functions
---
+++ decode/utils/multiprocessing/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+# submodules
+from .classes import *
+del classes
