98b7038018979976a42ee44da75d1b66f825394a
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-08 15:02:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 # coding: utf-8
 
+"DESHIMA Code for data analysis"
+
 # standard library
 from setuptools import setup, find_packages
+
+# dependent packages
+import decode as dc
 
 
 # main
 setup(
     name = 'decode',
-    version = '0.0',
+    description = __doc__,
+    version = dc.__version__,
+    author = dc.__author__,
+    author_email = dc.__email__,
     url = 'https://github.com/deshima-dev/decode',
+    install_requires = ['astropy', 'numpy', 'xarray'],
     packages = find_packages(),
 )
