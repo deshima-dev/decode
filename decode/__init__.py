b5930754ab353f641f2030b691386f1c8955a4f4
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-19 02:43:47 +0900

    Update __init__.py

--- decode/__init__.py
+++ decode/__init__.py
@@ -12,6 +12,7 @@ from . import utils
 # default logger
 import logging
 logger = logging.getLogger('decode')
+logger.propagate = False
 setlogfile(logger=logger)
 setloglevel(logger=logger)
 del logging
