12209c115437c2ec3bc47605c2762696fd6e07c7
Tsuyoshi Ishida <tishida@ioa.s.u-tokyo.ac.jp>
2017-10-06 17:57:59 +0900

    Implemented cube

--- decode/core/cube/functions.py
+++ decode/core/cube/functions.py
@@ -1,1 +1,43 @@
 # coding: utf-8
+
+# dependent packages
+import decode as dc
+import numpy as np
+import xarray as xr
+
+
+# functions
+def cube(data, xcoords=None, ycoords=None, chcoords=None, scalarcoords=None, attrs=None, name=None):
+    """Create a cube as an instance of xarray.DataArray with Decode accessor.
+
+    Args:
+        data (numpy.ndarray): A 3D (x x y x channel) array.
+        xcoords (dict, optional): A dictionary of arrays that label x axis.
+        ycoords (dict, optional): A dictionary of arrays that label y axis.
+        chcoords (dict, optional): A dictionary of arrays that label channel axis.
+        scalarcoords (dict, optional): A dictionary of values that don't label any axes (point-like).
+        attrs (dict, optional): A dictionary of attributes to add to the instance.
+        name (str, optional): A string that names the instance.
+
+    Returns:
+        cube (xarray.DataArray): a cube.
+
+    """
+    # initialize coords with default values
+    cube = xr.DataArray(data, dims=('x', 'y', 'ch'), attrs=attrs, name=name)
+    cube.dcc._initcoords()
+
+    # update coords with input values (if any)
+    if xcoords is not None:
+        cube.coords.update({key: ('t', xcoords[key]) for key in xcoords})
+
+    if ycoords is not None:
+        cube.coords.update({key: ('t', ycoords[key]) for key in ycoords})
+
+    if chcoords is not None:
+        cube.coords.update({key: ('ch', chcoords[key]) for key in chcoords})
+
+    if scalarcoords is not None:
+        cube.coords.update(scalarcoords)
+
+    return cube
