c5fbb3d17b17283dff4d19b4f4720bde0e6d2ce8
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-10-19 13:08:51 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,33 @@
 # standard library
 from setuptools import setup, find_packages
 
+install_requires = [
+    'astropy',
+    'numpy',
+    'scipy',
+    'pyyaml',
+    'xarray',
+]
+
+packages = [
+    'decode',
+    'decode.core',
+    'decode.core.array',
+    'decode.core.cube',
+    'decode.io',
+    'decode.logging',
+    'decode.utils',
+    'decode.utils.misc',
+]
 
 # main
 setup(
     name = 'decode',
     description = __doc__,
-    version = '0.1.1',
+    version = '0.1.3',
     author = 'snoopython',
     author_email = 'taniguchi@ioa.s.u-tokyo.ac.jp',
     url = 'https://github.com/deshima-dev/decode',
-    install_requires = ['astropy', 'numpy', 'scipy', 'pyyaml', 'xarray'],
-    packages = find_packages(),
+    install_requires = install_requires,
+    packages = packages,
 )
