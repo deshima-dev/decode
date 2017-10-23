011f7a612577da30817ad171d54d2a2c43d3cea1
Tsuyoshi Ishida <tishida@ioa.s.u-tokyo.ac.jp>
2017-10-23 23:31:53 +0900

    Updated docstrings of plotspectrum

--- decode/plot/functions.py
+++ decode/plot/functions.py
@@ -17,6 +17,29 @@ from astropy.io import fits
 
 # functions
 def plotspectrum(dataarray, shape, **kwargs):
+    """Plot a spectrum diagram.
+
+    Args:
+        dataarray (xarray.DataArray): Dataarray with which the spectrum is plotted.
+        shape (str): The shape of mask, 'box' or 'circle'.
+        kwargs (optional):
+            When 'box' is specified as shape,
+                xc: Center of x.
+                yc: Center of y.
+                width: Width.
+                height: Height.
+                xmin: Minimum of x.
+                xmax: Maximum of x.
+                ymin: Minimum of y.
+                ymax: Maximum of y.
+            When 'circle' is specified as shape,
+                xc: Center of x.
+                yc: Center of y.
+                radius: Radius.
+
+    Notes:
+        All kwargs should be specified as pixel coordinates.
+    """
     if dataarray.type == 'dca':
         pass
     elif dataarray.type == 'dcc':
