40aefc527b792bfdf478e49fb77108f1f4493e01
Tsuyoshi Ishida <tishida@ioa.s.u-tokyo.ac.jp>
2017-11-02 01:08:25 -0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ PACKAGES = [
 setup(
     name = 'decode',
     description = __doc__,
-    version = '0.2.5',
+    version = '0.2.6',
     author = 'snoopython',
     author_email = 'taniguchi@ioa.s.u-tokyo.ac.jp',
     url = 'https://github.com/deshima-dev/decode',
