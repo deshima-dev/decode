cf8a7c596c0718ef1759ef29a1d3338e79cd08ef
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-09-03 02:25:06 +0900

    Add test scripts

---
+++ tests/test_metadata.py
@@ -0,0 +1,11 @@
+import decode as dc
+
+
+def test_version():
+    """Make sure the version is valid."""
+    assert dc.__version__ == '0.5.4'
+
+
+def test_author():
+    """Make sure the author is valid."""
+    assert dc.__author__ == 'DESHIMA software team'
