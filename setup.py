f0789549e885555a1c652a6079024ff0b63603ee
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-09-03 02:12:37 +0900

    Update setup config

--- setup.py
+++ setup.py
@@ -1,43 +1,7 @@
-# standard library
+# dependent packages
 from setuptools import setup
 
-# module constants
-INSTALL_REQUIRES = [
-    'numpy',
-    'scipy',
-    'astropy',
-    'xarray',
-    'matplotlib',
-    'scikit-learn',
-    'pyyaml',
-    'tqdm',
-    'netCDF4',
-]
 
-PACKAGES = [
-    'decode',
-    'decode.core',
-    'decode.core.array',
-    'decode.core.cube',
-    'decode.io',
-    'decode.logging',
-    'decode.plot',
-    'decode.models',
-    'decode.utils',
-    'decode.utils.misc',
-    'decode.utils.ndarray'
-]
-
-
-# main
-setup(
-    name = 'decode',
-    description = 'DESHIMA code for data analysis',
-    version = '0.5.4',
-    author = 'DESHIMA software team',
-    author_email = 'taniguchi@a.phys.nagoya-u.ac.jp',
-    url = 'https://github.com/deshima-dev/decode',
-    install_requires = INSTALL_REQUIRES,
-    packages = PACKAGES,
-    package_data = {'decode': ['data/*.yaml']},
-)
+# main function
+if __name__ == '__main__':
+    setup()
