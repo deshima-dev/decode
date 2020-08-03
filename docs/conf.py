2203486229e709d20c6533bbcbcde0b2ec81a59c
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-03 17:02:45 +0900

    Update year period of copyright

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'De:code'
-copyright = '2019, DESHIMA software team'
+copyright = '2018-2020 DESHIMA software team'
 author = 'DESHIMA software team'
 
 # The full version, including alpha/beta/rc tags
