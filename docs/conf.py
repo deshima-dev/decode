43b531d355fcfe647dce5cb7df7ac57489ad6538
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-03 18:05:05 +0900

    #9 Format codes outside the package

--- docs/conf.py
+++ docs/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'De:code'
-copyright = '2018-2020 DESHIMA software team'
-author = 'DESHIMA software team'
+project = "De:code"
+copyright = "2018-2020 DESHIMA software team"
+author = "DESHIMA software team"
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.6'
+release = "0.5.6"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,19 +32,19 @@ release = '0.5.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,9 +52,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,9 +3,9 @@ import decode as dc
 
 def test_version():
     """Make sure the version is valid."""
-    assert dc.__version__ == '0.5.6'
+    assert dc.__version__ == "0.5.6"
 
 
 def test_author():
     """Make sure the author is valid."""
-    assert dc.__author__ == 'DESHIMA software team'
+    assert dc.__author__ == "DESHIMA software team"
