7c93512d1b7c2144c9c2688b64f4ec5e58f39275
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-11-29 06:41:06 +0000

    #39 Update package version (0.6.0 → 0.6.1)

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,7 +3,7 @@ import decode as dc
 
 def test_version():
     """Make sure the version is valid."""
-    assert dc.__version__ == "0.6.0"
+    assert dc.__version__ == "0.6.1"
 
 
 def test_author():
