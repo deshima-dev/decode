1c3ea1c64cf036f9a35ed45b0a813b748e9041bc
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-24 13:06:46 +0000

    #79 Add function for plotting 1D or 2D data of DEMS or its coordinate

--- decode/plot.py
+++ decode/plot.py
@@ -1,13 +1,17 @@
-__all__ = ["state"]
+__all__ = ["data", "state"]
 
 
 # standard library
-from typing import Any, Literal, Optional
+from typing import Any, Literal
 
 
 # dependencies
 import xarray as xr
 from matplotlib.artist import Artist
+
+
+# constants
+DEMS_DIMS = "time", "chan"
 
 
 def state(
@@ -50,3 +54,51 @@ def state(
         return dems.lat.plot.scatter(**options)
 
     raise ValueError("On must be either time or sky.")
+
+
+def data(
+    data: xr.DataArray,
+    *,
+    squeeze: bool = True,
+    **options: Any,
+) -> Artist:
+    """Plot 1D or 2D data of DEMS or its coordinate.
+
+    Args:
+        data: DEMS DataArray or coordinate DataArray.
+
+    Keyword Args:
+        squeeze: Whether to squeeze the data before plotting.
+        options: Plotting options to be passed to Matplotlib.
+
+    Returns:
+        Matplotlib artist object of the plotted data.
+
+    """
+    if squeeze:
+        data = data.squeeze()
+
+    if data.dims == (DEMS_DIMS[0],):
+        options = {
+            "edgecolors": "none",
+            "hue": "state",
+            "s": 3,
+            "x": "time",
+            **options,
+        }
+        return data.plot.scatter(**options)  # type: ignore
+
+    if data.dims == (DEMS_DIMS[1],):
+        options = {
+            "edgecolors": "none",
+            "hue": "state",
+            "s": 3,
+            "x": "d2_mkid_frequency",
+            **options,
+        }
+        return data.plot.scatter(**options)  # type: ignore
+
+    if data.dims == DEMS_DIMS:
+        return data.plot.pcolormesh(**options)  # type: ignore
+
+    raise ValueError(f"Dimensions must be subset of {DEMS_DIMS}.")
