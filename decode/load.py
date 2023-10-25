85629c74f22b748747929e0d6be8232a9e678974
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-25 13:27:11 +0000

    #94 Add function for loading ALMA ATM model

--- decode/load.py
+++ decode/load.py
@@ -1,20 +1,69 @@
-__all__ = ["dems"]
+__all__ = ["atm", "dems"]
 
 
 # standard library
 from pathlib import Path
-from typing import Any, Union
+from typing import Any, Literal, Union
+from warnings import catch_warnings, simplefilter
 
 
 # dependencies
+import numpy as np
+import pandas as pd
 import xarray as xr
 
 
 # constants
+ALMA_ATM = "alma_atm.txt"
+DATA_DIR = Path(__file__).parent / "data"
 NETCDF_ENGINE = "scipy"
 NETCDF_SUFFIX = ".nc"
 ZARR_ENGINE = "zarr"
 ZARR_SUFFIX = ".zarr"
+
+
+def atm(*, type: Literal["eta", "tau"] = "tau") -> xr.DataArray:
+    """Load an ALMA ATM model as a DataArray.
+
+    Args:
+        type: Type of model to be stored in the DataArray.
+            Either ``'eta'`` (transmission) or ``'tau'`` (opacity).
+
+    Returns:
+        DataArray that stores the ALMA ATM model.
+
+    """
+    atm = pd.read_csv(
+        DATA_DIR / ALMA_ATM,
+        comment="#",
+        index_col=0,
+        sep=r"\s+",
+    )
+    freq = xr.DataArray(
+        atm.index * 1e9,
+        dims="freq",
+        attrs={
+            "long_name": "Frequency",
+            "units": "Hz",
+        },
+    )
+    pwv = xr.DataArray(
+        atm.columns.astype(float),
+        dims="pwv",
+        attrs={
+            "long_name": "Precipitable water vapor",
+            "units": "mm",
+        },
+    )
+
+    if type == "eta":
+        return xr.DataArray(atm, coords=(freq, pwv))
+    elif type == "tau":
+        with catch_warnings():
+            simplefilter("ignore")
+            return xr.DataArray(-np.log(atm), coords=(freq, pwv))
+    else:
+        raise ValueError("Type must be either eta or tau.")
 
 
 def dems(dems: Union[Path, str], /, **options: Any) -> xr.DataArray:
