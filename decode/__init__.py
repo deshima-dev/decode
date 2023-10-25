c80f856ca538e279721e5fea98ce14f989fa2932
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-25 05:10:25 +0000

    #95 Add empty assign module

--- decode/__init__.py
+++ decode/__init__.py
@@ -1,8 +1,14 @@
-__all__ = ["load", "plot", "select"]
+__all__ = [
+    "assign",
+    "load",
+    "plot",
+    "select",
+]
 __version__ = "2.2.0"
 
 
 # submodules
+from . import assign
 from . import load
 from . import plot
 from . import select
