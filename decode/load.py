8c5be60ba6bf013ac75a6ab4f3333099faa656ce
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-25 01:35:52 +0000

    #89 Update function for loading DEMS

--- decode/load.py
+++ decode/load.py
@@ -1,4 +1,4 @@
-__all__ = ["open_dems"]
+__all__ = ["dems"]
 
 
 # standard library
@@ -17,31 +17,39 @@ ZARR_ENGINE = "zarr"
 ZARR_SUFFIX = ".zarr"
 
 
-def open_dems(dems: Union[Path, str], **kwargs: Any) -> xr.DataArray:
-    """Open a DEMS file as a DataArray.
+def dems(dems: Union[Path, str], /, **options: Any) -> xr.DataArray:
+    """Load a DEMS file as a DataArray.
 
     Args:
         dems: Path of the DEMS file.
-        kwargs: Arguments to be passed to ``xarray.open_dataarray``.
+
+    Keyword Args:
+        options: Arguments to be passed to ``xarray.open_dataarray``.
 
     Return:
-        A DataArray of the opened DEMS file.
+        Loaded DEMS DataArray.
 
     Raises:
         ValueError: Raised if the file type is not supported.
 
     """
-    engine: str
     suffixes = Path(dems).suffixes
 
     if NETCDF_SUFFIX in suffixes:
-        engine = kwargs.pop("engine", NETCDF_ENGINE)
+        options = {
+            "engine": NETCDF_ENGINE,
+            **options,
+        }
     elif ZARR_SUFFIX in suffixes:
-        engine = kwargs.pop("engine", ZARR_ENGINE)
+        options = {
+            "chunks": "auto",
+            "engine": ZARR_ENGINE,
+            **options,
+        }
     else:
         raise ValueError(
             f"File type of {dems} is not supported."
             "Use netCDF (.nc) or Zarr (.zarr, .zarr.zip)."
         )
 
-    return xr.open_dataarray(dems, engine=engine, **kwargs)
+    return xr.open_dataarray(dems, **options)
