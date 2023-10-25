6b934fea4e38b00891382b380d63d6c70cfdccd7
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-10-25 06:40:16 +0000

    #95 Add test for assign module

---
+++ tests/test_assign.py
@@ -0,0 +1,16 @@
+# standard library
+from pathlib import Path
+
+
+# dependencies
+from decode import assign, load
+
+
+# constants
+DEMS_DIR = Path(__file__).parents[1] / "data" / "dems"
+DEMS = load.dems(DEMS_DIR / "dems_20171111110002.nc.gz")
+
+
+def test_assign_scan() -> None:
+    assert assign.scan(DEMS).scan.min() == 0
+    assert assign.scan(DEMS).scan.max() == 244
