42db06cf821c136050e4148bfee92324dbd1c9d0
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-11-06 16:13:53 +0000

    #106 Add function to convert skycoord frame

--- decode/convert.py
+++ decode/convert.py
@@ -1,4 +1,4 @@
-__all__ = ["units"]
+__all__ = ["frame", "units"]
 
 
 # standard library
@@ -6,6 +6,7 @@ from typing import Optional, Union
 
 
 # dependencies
+import numpy as np
 import xarray as xr
 from astropy.units import Equivalency, Quantity, Unit
 
@@ -13,6 +14,51 @@ from astropy.units import Equivalency, Quantity, Unit
 # type hints
 UnitLike = Union[Unit, str]
 
+
+def frame(
+    dems: xr.DataArray,
+    new_frame: str,
+    /,
+    *,
+    inplace: bool = False,
+) -> xr.DataArray:
+    """Convert skycoord frame of DEMS.
+
+    Args:
+        dems: Target DEMS DataArray.
+        new_frame: Skycoord frame to be converted from the current ones.
+        inplace: Whether the skycoord frame are converted in-place.
+
+    Returns:
+        DEMS DataArray with the skycoord frame converted.
+
+    """
+    if not inplace:
+        # deepcopy except for data
+        dems = dems.copy(data=dems.data)
+
+    if not new_frame == "relative":
+        raise ValueError("Relative is only available.")
+
+    lon, lon_origin = dems["lon"], dems["lon_origin"]
+    lat, lat_origin = dems["lat"], dems["lat_origin"]
+    cos = np.cos(Quantity(lat, lat.attrs["units"]).to("rad"))
+
+    if lon.attrs["units"] != lon_origin.attrs["units"]:
+        raise ValueError("Units of lon and lon_origin must be same.")
+
+    if lat.attrs["units"] != lat_origin.attrs["units"]:
+        raise ValueError("Units of lat and lat_origin must be same.")
+
+    # do not change the order below!
+    lon -= lon_origin
+    lon *= cos
+    lat -= lat_origin
+    lon_origin[:] = 0.0
+    lat_origin[:] = 0.0
+
+    dems["frame"] = "relative-" + dems["frame"]
+    return dems
 
 def units(
     dems: xr.DataArray,
