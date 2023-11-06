5bc8621b645b256b4d488f076d61968514c50db7
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-11-06 16:24:25 +0000

    #106 Update test for convert module

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -11,6 +11,13 @@ DEMS_DIR = Path(__file__).parents[1] / "data" / "dems"
 DEMS = load.dems(DEMS_DIR / "dems_20171111110002.nc.gz")
 
 
+def test_convert_frame() -> None:
+    converted = convert.frame(DEMS, "relative")
+    assert (converted.lon_origin == 0.0).all()
+    assert (converted.lat_origin == 0.0).all()
+    assert (converted.frame == "relative-altaz").all()
+
+
 def test_convert_units() -> None:
     converted = convert.units(DEMS, "interval", "hr")
     assert (DEMS.interval / 3600 == converted.interval).all()
