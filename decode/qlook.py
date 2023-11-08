acf3bc429e4cb4f167b44e873e2b1ae142f9d05c
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-11-08 22:23:36 +0000

    #124 Add zscan command

--- decode/qlook.py
+++ decode/qlook.py
@@ -95,7 +95,76 @@ def skydip(
     print(str(result))
 
 
+def zscan(
+    dems: Path,
+    /,
+    *,
+    include_mkid_ids: Optional[Sequence[int]] = None,
+    exclude_mkid_ids: Optional[Sequence[int]] = BAD_MKID_IDS,
+    data_type: Literal["df/f", "brightness"] = "brightness",
+    outdir: Path = Path(),
+    format: str = "png",
+) -> None:
+    """Quick-look at an observation of subref axial focus scan.
+
+    Args:
+        dems: Input DEMS file (netCDF or Zarr).
+        include_mkid_ids: MKID IDs to be included in analysis.
+            Defaults to all MKID IDs.
+        exclude_mkid_ids: MKID IDs to be excluded in analysis.
+            Defaults to bad MKID IDs found on 2023-11-07.
+        data_type: Data type of the input DEMS file.
+        outdir: Output directory for analysis results.
+        format: Output image format of analysis results.
+
+    """
+    dems = Path(dems)
+    result = Path(outdir) / dems.with_suffix(f".zscan.{format}").name
+
+    # load DEMS
+    da = load.dems(dems, chunks=None)
+
+    if data_type == "df/f":
+        da.attrs.update(long_name=data_type, units="dimensionless")
+
+    # select DEMS
+    da = select.by(da, "d2_mkid_type", include="filter")
+    da = select.by(
+        da,
+        "d2_mkid_id",
+        include=include_mkid_ids,
+        exclude=exclude_mkid_ids,
+    )
+    da_on = select.by(da, "state", include="ON")
+    da_off = select.by(da, "state", exclude="GRAD")
+
+    # plotting
+    weight = da_off.std("time") ** -2
+    series = (da_on * weight).sum("chan") / weight.sum("chan")
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+
+    ax = axes[0]
+    plot.data(np.abs(series), hue="aste_subref_z", ax=ax)
+    ax.set_title(Path(dems).name)
+    ax.grid(True)
+
+    ax = axes[1]
+    plot.data(np.abs(series), x="aste_subref_z", ax=ax)
+    ax.set_title(Path(dems).name)
+    ax.grid(True)
+
+    fig.tight_layout()
+    fig.savefig(result)
+    print(str(result))
+
+
 def main() -> None:
     """Entry point of the decode-qlook command."""
     with xr.set_options(keep_attrs=True):
-        Fire({"skydip": skydip})
+        Fire(
+            {
+                "skydip": skydip,
+                "zscan": zscan,
+            }
+        )
