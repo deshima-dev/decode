e33041eb7ad1786d1eaa182c86e9fa0013dd0dd4
Fujita Shinji <109708402+ShinjiFujita@users.noreply.github.com>
2024-08-06 17:43:36 +0900

    Update fit.py

--- decode/fit.py
+++ decode/fit.py
@@ -122,3 +122,20 @@ def cube(
         },
         errors="ignore",
     )
+
+
+def gaussian_2d(xy, amp, x0, y0, sigma_x, sigma_y, theta, offset):
+    x, y = xy
+    x0 = float(x0)
+    y0 = float(y0)
+    a = (np.cos(theta) ** 2) / (2 * sigma_x**2) + (np.sin(theta) ** 2) / (
+        2 * sigma_y**2
+    )
+    b = -(np.sin(2 * theta)) / (4 * sigma_x**2) + (np.sin(2 * theta)) / (4 * sigma_y**2)
+    c = (np.sin(theta) ** 2) / (2 * sigma_x**2) + (np.cos(theta) ** 2) / (
+        2 * sigma_y**2
+    )
+    g = offset + amp * np.exp(
+        -(a * ((x - x0) ** 2) + 2 * b * (x - x0) * (y - y0) + c * ((y - y0) ** 2))
+    )
+    return g.ravel()
